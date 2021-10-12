52011f2cbecc2b518f5509c698758e8b54ed2ede
Sam Perry <17242713+sdp8483@users.noreply.github.com>
2021-10-12 19:43:44 -0400

    init code

--- firmware/shutter_tester/src/main.cpp
+++ firmware/shutter_tester/src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
 
+#define INPUT_PIN  2
+
+volatile unsigned long edge_t_us[2];
+volatile int edge_i = 0;
+
+void interupt(void);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(INPUT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INPUT_PIN), interupt, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (edge_i == 1) {
+    detachInterrupt(digitalPinToInterrupt(INPUT_PIN));
+
+    unsigned long t = edge_t_us[1] - edge_t_us[0];
+
+    Serial.print(F("t0: "));
+    Serial.print(edge_t_us[0]);
+    Serial.print(F("us t1: "));
+    Serial.print(edge_t_us[1]);
+    Serial.print(F("us td: "));
+    Serial.print(t);
+    Serial.println(F("us"));
+
+    edge_i = 0;
+
+    attachInterrupt(digitalPinToInterrupt(INPUT_PIN), interupt, CHANGE);
+  }
+}
+
+void interupt(void) {
+  edge_t_us[edge_i] = micros();
+  edge_i++;
 }
